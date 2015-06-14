a9def1dd47959fef5bede906a264d3506a358416
IvanGuardado <contact@ivanguardado.com>
2015-06-14 14:29:49 +0200

    Includes mootools-core and mootools-more with webpack

--- public/app/main.js
+++ public/app/main.js
@@ -1,18 +1,3 @@
-var Router = require('./lib/router');
-var routes = require('./routes');
-
-var Mainapp = new Class({
-  initialize: function(){
-    this.router = new Router();
-    this.router.set(routes);
-    this.router.start();
-  },
-
-  test: function(){
-    console.log('test');
-  }
-});
-
-
-module.exports = new Mainapp();
-
+require('./vendor/mootools-loader');
+require('./app');
+require('./legacy');
